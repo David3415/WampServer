b2e687ea6d9b427805a5496dc2dbc6fb57f77d35
David <Dgagiev@gmail.com>
2024-06-22 19:27:03 +0300

    Git 6 проект запускается и выдаёт log

--- app/src/main/java/com/example/wampserver/MainActivity.kt
+++ app/src/main/java/com/example/wampserver/MainActivity.kt
@@ -1,47 +1,28 @@
 package com.example.wampserver
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.wampserver.ui.theme.WampServerTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var mainApi: MainApi
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WampServerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+       // enableEdgeToEdge()
+        CoroutineScope(Dispatchers.IO).launch {
+            val list = mainApi.getAllUsers()
+            list.forEach { user -> Log.d("MyLog", "User: ${user.name}") }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WampServerTheme {
-        Greeting("Android")
-    }
-}
